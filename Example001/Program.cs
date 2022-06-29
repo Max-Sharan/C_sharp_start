50749f5b215cffb8cd65973d9805239968fb904c
Max Sharan <qshark55@gmail.com>
2022-06-30 00:26:40 +0300

    add my name and C#

--- Example001/Program.cs
+++ Example001/Program.cs
@@ -1,3 +1,3 @@
 ﻿Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+Console.WriteLine("My name is Maxim. :-)");
+Console.WriteLine("I'm proggramming in C#. ;-)");
