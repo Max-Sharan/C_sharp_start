4eea12fe7c6ae7352e0c2afab529cc5325cf02b8
Max Sharan <qshark55@gmail.com>
2022-06-30 00:17:07 +0300

    add random numbers

--- Example003/Program.cs
+++ Example003/Program.cs
@@ -1,5 +1,6 @@
-﻿int numberA = 7;
-int numberB = 5;
+﻿int numberA = new Random().Next(0, 15); // 0,1,2,...14
+Console.WriteLine("1-е случайное число = " + numberA);
+int numberB = new Random().Next(0, 15);
+Console.WriteLine("2-е случайное число = " + numberB);
 int result = numberA + numberB;
-Console.Write("Сумма чисел = ");
-Console.WriteLine(result);
+Console.WriteLine("Сумма чисел = " + result);
