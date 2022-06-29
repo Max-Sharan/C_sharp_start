bb1263100fc8d8c5344ce6e41688b6ee6dc0505b
Max Sharan <qshark55@gmail.com>
2022-06-30 00:48:59 +0300

    Example 005 add condition If-Else

---
+++ Example005_if-else/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите пожалуйста своё имя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же Маша! Наконец-то ты пришла!");
+}
+else
+{
+    Console.WriteLine("Привет, " + username + "!");
+}
