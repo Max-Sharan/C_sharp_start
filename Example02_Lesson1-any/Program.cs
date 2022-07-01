e3fe9422bad03c2f795331943d86f0605d58cfff
Max Sharan <qshark55@gmail.com>
2022-07-01 23:13:16 +0300

    Lesson 1 and of number and array of number

---
+++ Example02_Lesson1-any/Program.cs
@@ -0,0 +1,19 @@
+﻿// -------------------- Вывод последней цифры терхзначного числа ---------------- //
+
+Console.Write("Введите целое трехзначное число (от 100 до 999): ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int thirdNumber = number % 10;
+
+Console.WriteLine("Последняя цифра числа " + number + " - это " + thirdNumber);
+
+// -------------------- Вывод последовательности от -N до N {-2, -1, 0, 1, 2} ---------------- //
+
+Console.Write("Введите целое число (от 1 до 20): ");
+int num = Convert.ToInt32(Console.ReadLine());
+int i = -num;
+while (i <= num)
+{
+    Console.Write(i + ", ");
+    i++;
+}
