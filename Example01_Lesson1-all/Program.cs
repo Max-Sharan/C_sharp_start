4bb5c55df939b449505ff2b5541b28a870ddbacb
Max Sharan <qshark55@gmail.com>
2022-07-01 21:50:53 +0300

    Lesson 1 check quad of number

--- Example01_Lesson1-all/Program.cs
+++ Example01_Lesson1-all/Program.cs
@@ -1,12 +1,27 @@
-﻿int a = new Random().Next(1, 10);
-Console.Write("Введите число: ");
-int b = Convert.ToInt32(Console.ReadLine());
+﻿// int a = new Random().Next(1, 10);
+// Console.Write("Введите число: ");
+// int b = Convert.ToInt32(Console.ReadLine());
 
-int quad = a * a;
-int quadro = b * b; 
-Console.WriteLine("Квадрат числа " + a + " равен " + quad);
-Console.WriteLine("Квадрат числа " + b + " равен " + quadro);
+// int quad = a * a;
+// int quadro = b * b; 
+// Console.WriteLine("Квадрат числа " + a + " равен " + quad);
+// Console.WriteLine("Квадрат числа " + b + " равен " + quadro);
 
-// ---------------------------------------------------------- //
+// ----------------------- Проверяем квадрат числа -------------------------------------- //
 
+Console.Write("Введите целое число x: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите целое число y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+int xQuad = x * x;
+
+if (y == xQuad)
+{
+    Console.Write("y = " + y + " - это квадрат числа x = " + x);
+}
+else 
+{
+    Console.Write("y = " + y + ", не является квадратом числа x = " + x);
+}
+// ----------------------- Выводим дни недели -------------------------------------- //
 
