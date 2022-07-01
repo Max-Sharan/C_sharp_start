22c92c64422193090e93018eaa3b6195c5748a47
Max Sharan <qshark55@gmail.com>
2022-07-01 20:55:42 +0300

    Example 01 Lesson 1 first work quad for number

---
+++ Example01_Lesson1-all/Program.cs
@@ -0,0 +1,12 @@
+﻿int a = new Random().Next(1, 10);
+Console.Write("Введите число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int quad = a * a;
+int quadro = b * b; 
+Console.WriteLine("Квадрат числа " + a + " равен " + quad);
+Console.WriteLine("Квадрат числа " + b + " равен " + quadro);
+
+// ---------------------------------------------------------- //
+
+
