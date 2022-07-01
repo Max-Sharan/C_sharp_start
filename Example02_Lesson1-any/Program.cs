6d70864eb526d1ce2a8f5c7e9b074796d93b7459
Max Sharan <qshark55@gmail.com>
2022-07-02 02:34:32 +0300

    Lesson 1 add while to compare user number with the want number and formating results

--- Example02_Lesson1-any/Program.cs
+++ Example02_Lesson1-any/Program.cs
@@ -1,19 +1,32 @@
 ﻿// -------------------- Вывод последней цифры терхзначного числа ---------------- //
+Console.Clear();
 
 Console.Write("Введите целое трехзначное число (от 100 до 999): ");
 int number = Convert.ToInt32(Console.ReadLine());
 
+while(number > 999 || number < 100)
+{
+    Console.WriteLine();
+    Console.WriteLine("Надо было ввести число (от 100 до 999)");
+    Console.Write("Повторите ввод числа (от 100 до 999): ");
+    number = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine();
+}
+Console.WriteLine();
 int thirdNumber = number % 10;
-
 Console.WriteLine("Последняя цифра числа " + number + " - это " + thirdNumber);
+Console.WriteLine("------------------------------------------------------");
 
 // -------------------- Вывод последовательности от -N до N {-2, -1, 0, 1, 2} ---------------- //
-
+Console.WriteLine("Следующий пример: вывод последовательности чисел");
+Console.WriteLine();
 Console.Write("Введите целое число (от 1 до 20): ");
 int num = Convert.ToInt32(Console.ReadLine());
 int i = -num;
 while (i <= num)
 {
-    Console.Write(i + ", ");
+    Console.Write(i + " ");
     i++;
 }
+Console.WriteLine();
+Console.WriteLine("------------------------------------------------------");
